ae3c66abddf516444085edbad7b0ef1c49e6993e
wujianguo <wujianguo@haixue.com>
2023-03-10 09:37:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <p>{{ data.data }}</p>
+    <div class="wrapper">
+      <HelloWorld msg='Hello World' />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      data: { data: 'Loading...'}
+    }
+  },
+  mounted() {
+    axios.get('https://vercel-temp2.vercel.app/api/hello').then((response) => {
+      this.data = response.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
